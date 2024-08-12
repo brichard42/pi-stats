b228e79a5622d76cc537a4f512e649e080c6eaef
brichard <bastien.richardcana@gmail.com>
2024-08-12 17:34:05 +0200

    prod

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>
-        Brichard's Raspberry PI
+        Brichard&#39;s Raspberry PI
       </h1>
     </main>
   );
